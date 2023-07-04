8e2cef9eb90a9be97bb04e66ae41b29762919edd
Andrea Monterrosa <moralesandrea.edr4@gmail.com>
2023-07-04 10:12:40 -0600

    Trying out ScaffoldMessenger on dice pressed

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,11 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
-      backgroundColor: Colors.red,
+      backgroundColor: Colors.green.shade700,
       appBar: AppBar(
         title: Text("Dicee"),
         centerTitle: true,
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.green.shade900,
       ),
       body: DicePage(),
     ),
@@ -19,22 +19,42 @@ class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Image.asset("images/dice1.png"),
+      child: Center(
+        child: Row(
+          children: [
+            Expanded(
+              child: TextButton(
+                onPressed: () {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: const Text('Awesome Snackbar!'),
+                      duration: Duration(seconds: 5),
+                      backgroundColor: Colors.deepPurple,
+                      behavior: SnackBarBehavior.floating,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      action: SnackBarAction(
+                        label: 'OK',
+                        textColor: Colors.white,
+                        onPressed: () {
+                          // Code to execute.
+                        },
+                      ),
+                    ),
+                  );
+                },
+                child: Image.asset("images/dice1.png"),
+              ),
             ),
-          ),
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Image.asset("images/dice1.png"),
+            Expanded(
+              child: TextButton(
+                onPressed: () {},
+                child: Image.asset("images/dice1.png"),
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
